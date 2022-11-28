a91004640cb37bbb9bed628ab06a5c151d56e15c
ido177 <63454209+ido177@users.noreply.github.com>
2022-11-28 20:54:31 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
-import pyspark.sql.types as T   # let us cast into understandable for spark type 
+import pyspark.sql.types as T
 
 def main():
     spark = SparkSession.builder.getOrCreate()
@@ -11,7 +11,7 @@ def main():
         df
         .groupBy('manufacturer_name')
         .agg(
-            F.count("manufacturer_name").alias("count"), # alias allows to rename columns
+            F.count("manufacturer_name").alias("count"),
             F.round(F.avg("year_produced")).cast(T.IntegerType()).alias("avg_year"), 
             F.min(F.col("price_usd").cast(T.FloatType())).alias("min_price"),
             F.max(F.col("price_usd").cast(T.FloatType())).alias("max_price")
